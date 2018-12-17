854c5fb6ee79b89680970fe78eb49ee8a5e08483
richyliu <richy.liu.2002@gmail.com>
2018-12-17 14:41:34 -0800

    remove unnecessary favicons and build

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         extensions: ['.ts', '.tsx']
     },
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, '/docs'),
         filename: 'bundle.min.js'
     },
     module: {
